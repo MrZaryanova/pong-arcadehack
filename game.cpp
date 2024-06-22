e11186b4afacdb11da2e4a191cfb96b206ee382c
MrZaryanova <149221093+MrZaryanova@users.noreply.github.com>
2024-06-22 14:51:06 -0400

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -6,7 +6,31 @@ int main() {
     if (!ballTexture.loadFromFile("ball.png")) {
     window.close();
 }
-sf::Sprite ball;
-ball.setTexture(ballTexture);
+  sf::Sprite ball;
+  ball.setTexture(ballTexture);
+  ball.setPosition(sf::Vector2f(0,0));
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear(sf::Color::Cyan);
+      
+
+        // start of frame
+      
+        window.draw(ball);
+
+
+        // end of frame
+        window.display();
+    }
+
+    return 0;
+
 
 }
